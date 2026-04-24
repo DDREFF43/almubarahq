38bd2cdbcc57f3e1ad09fbec14cecf727e35bef6
raf-34 <rorbey970@gmail.com>
2026-04-24 11:16:54 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,19 +1,19 @@
 const databaseProyek = {
     "sistem_pos": {
-        judul: "Sisistem POS CLUB ARSENAL",
+        judul: "VS CODE",
         keahlian: ["JavaScript", "PHP", "MySQL"],
-        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT3HMS3uItnWuKMeEj3T_84uzjA6KSGq4pRA&s",
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThOU4hIRB20Oy0barVYD6x4N8hQa5QOW7Www&s",
         latar: "ini adalah proyek magang",
         tantangan: "ini sangat sulit",
         pelajaran: "saya belajar banyak tentang pemrograman",
         link: ""
     },
     "sistem_pos2": {
-        judul: "Sisistem POS CLUB ARSENAL",
+        judul: "JavaScript",
         keahlian: ["JavaScript", "PHP", "MySQL"],
-        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT3HMS3uItnWuKMeEj3T_84uzjA6KSGq4pRA&s",
+        gambar: "https://media.licdn.com/dms/image/v2/D4D12AQEkuWX8QaNlTA/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1721945848366?e=2147483647&v=beta&t=mR89ftGBaI7JdBdjIhTm8M0NcM9Oga8cUBVA-LIXisE",
         latar: "ini adalah proyek magang",
-        tantangan: "ini sangat sulit",
+        tantangan: "ini rumit",
         pelajaran: "saya belajar banyak tentang pemrograman",
         link: ""
     },
