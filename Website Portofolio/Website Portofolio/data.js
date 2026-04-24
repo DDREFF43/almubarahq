4630c9e1f9e460bcb205f4babac9d2d30216fe89
raf-34 <rorbey970@gmail.com>
2026-04-24 09:07:40 +0700

    Add files via upload

---
+++ Website Portofolio/Website Portofolio/data.js
@@ -0,0 +1,20 @@
+const databaseProyek = {
+    "sistem_pos": {
+        judul: "Sisistem POS CLUB ARSENAL",
+        keahlian: ["JavaScript", "PHP", "MySQL"],
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT3HMS3uItnWuKMeEj3T_84uzjA6KSGq4pRA&s",
+        latar: "ini adalah proyek magang",
+        tantangan: "ini sangat sulit",
+        pelajaran: "saya belajar banyak tentang pemrograman",
+        link: ""
+    },
+    "sistem_pos2": {
+        judul: "Sisistem POS CLUB ARSENAL",
+        keahlian: ["JavaScript", "PHP", "MySQL"],
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT3HMS3uItnWuKMeEj3T_84uzjA6KSGq4pRA&s",
+        latar: "ini adalah proyek magang",
+        tantangan: "ini sangat sulit",
+        pelajaran: "saya belajar banyak tentang pemrograman",
+        link: ""
+    },
+};
